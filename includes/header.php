bcf82037537aee11743bf966f9e2867512f1e361
Whasnat <w.hasnat94@gmail.com>
2019-06-27 03:26:03 +0600

    Added Navigation top-bar, logo and nav icons

--- includes/header.php
+++ includes/header.php
@@ -13,9 +13,42 @@ else{
 <html>
 <head>
 	<title>ScienceFeed</title>
+	<!--Javascript-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="assets/js/boorstrap.js"></script>
-
+	
+	<!--CSS-->
 	<link rel="stylesheet" type="text/css" href="assets/css/boorstrap.css">
+	<script src="https://kit.fontawesome.com/f44ce8d2ea.js"></script>
+
+	<!--<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/3.6.0/css/font-awesome.css" >-->
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	
 </head>
 <body>
+
+	<div class="top_bar">
+		<div class="logo">
+			<a href="index.php"><strong>Science Feed</strong></a>
+		</div>
+
+		<nav>
+			<a href="index.php">
+				<i class="fas fa-home"></i>
+			</a>
+			<a href="#">
+				<i class="fas fa-user-astronaut"></i>
+			</a>
+			<a href="#">
+				<i class="fas fa-bell"></i>
+			</a>
+			<a href="#">
+				<i class="fas fa-cog"></i>
+			</a>
+			<a href="#">
+				<i class="fas fa-comment-alt"></i>
+			</a>
+
+		</nav>
+	</div>
+
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 	include("includes/header.php");
 	// session_destroy();    //destroy session
  ?>
-	<h1>Hello to Science Feed</h1>
 
 </body>
 </html>
